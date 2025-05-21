3365dcf37a532b110f164c9247c53abea766fd60
santiagoduc0s <santidu200@gmail.com>
2025-05-21 03:39:07 +0200

    fix(validations): validations num values

--- lib/core/form/validators/validation_messages.dart
+++ lib/core/form/validators/validation_messages.dart
@@ -15,21 +15,23 @@ final Map<String, String Function(Object)> validationMessages = {
   ValidationMessage.equals: (error) =>
       Injector.findSingleton<Localization>().tr.validation_equals,
   ValidationMessage.max: (error) {
-    final max = (error as Map<String, int?>)['max']!;
+    final max = (error as Map<String, dynamic>)['max'] as int;
     return Injector.findSingleton<Localization>().tr.validation_max(max);
   },
   ValidationMessage.maxLength: (error) {
-    final requiredLength = (error as Map<String, int?>)['requiredLength']!;
+    final requiredLength =
+        (error as Map<String, dynamic>)['requiredLength'] as int;
     return Injector.findSingleton<Localization>()
         .tr
         .validation_maxLength(requiredLength);
   },
   ValidationMessage.min: (error) {
-    final min = (error as Map<String, int?>)['min']!;
+    final min = (error as Map<String, dynamic>)['min'] as int;
     return Injector.findSingleton<Localization>().tr.validation_min(min);
   },
   ValidationMessage.minLength: (error) {
-    final requiredLength = (error as Map<String, int?>)['requiredLength']!;
+    final requiredLength =
+        (error as Map<String, dynamic>)['requiredLength'] as int;
     return Injector.findSingleton<Localization>()
         .tr
         .validation_minLength(requiredLength);
